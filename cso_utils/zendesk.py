9b0051e062aba3d047fb705485840bf9e9e3ed65
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-03-17 15:28:09 -0700

    Update Zendesk class - Create a ticket with an internal message, then send the message to the customer. Tag, assign, and mark solved.

--- cso_utils/zendesk.py
+++ cso_utils/zendesk.py
@@ -1,15 +1,31 @@
-import zenpy, os
+import requests
 
 class Zendesk:
     def authenticate(self, subdomain: str, email: str, token: str) -> None:
-        self._zendesk = zenpy.Zenpy(subdomain=subdomain, email=email, token=token)
+        self._subdomain = subdomain
+        self._auth = (email + '/token', token)
 
-    def create_ticket_and_send_to_customer(self, email: str, subject: str, message: str) -> None:
-        """Create a new ticket and send the message to the customer."""
-        requester = zenpy.lib.api_objects.User(email=email)
-        ticket = zenpy.lib.api_objects.Ticket(subject=subject, 
-                                              description=message, 
-                                              requester=requester)
-        ticket_in_zendesk = self._zendesk.tickets.create(ticket)
-        ticket_in_zendesk.comment = zenpy.lib.api_objects.Comment(body=message, public=True)
-        self._zendesk.tickets.update(ticket_in_zendesk)
+    def create_ticket_and_send_to_customer(self, customer_email: str, subject: str, 
+                                           html_message: str, group_id: str,
+                                           tags: [str], assignee_email: str) -> str:
+        """Create a new ticket and send the message to the customer. 
+        Return the ID of the new ticket. 
+        """
+        url = f'https://{self._subdomain}.zendesk.com/api/v2/tickets'
+        data = {'ticket': {'subject': subject, 
+                           'requester': {'email': customer_email, 'verified': True}, 
+                           'assignee_email': assignee_email, 
+                           'comment': {'html_body': html_message, 'public': False}, 
+                           }}
+        response = requests.post(url, auth=self._auth, json=data)
+        response.raise_for_status()
+        ticket_id = str(response.json()['ticket']['id'])
+
+        response = requests.put(url + '/' + ticket_id, auth=self._auth, 
+                                json={'ticket': {'comment': {'html_body': html_message, 'public': True}, 
+                                                 'group_id': int(group_id), 
+                                                 'tags': tags,
+                                                 'status': 'solved'}})
+        response.raise_for_status()
+
+        return ticket_id
