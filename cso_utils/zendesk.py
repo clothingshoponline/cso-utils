01ea186753b3387f0de8728f7c4afc3723e164e0
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-03-18 11:45:33 -0700

    Make url into class attribute, delete extra comma

--- cso_utils/zendesk.py
+++ cso_utils/zendesk.py
@@ -4,6 +4,7 @@ class Zendesk:
     def authenticate(self, subdomain: str, email: str, token: str) -> None:
         self._subdomain = subdomain
         self._auth = (email + '/token', token)
+        self._url = f'https://{self._subdomain}.zendesk.com/api/v2/tickets'
 
     def create_ticket_and_send_to_customer(self, customer_email: str, subject: str, 
                                            html_message: str, group_id: str,
@@ -11,17 +12,15 @@ class Zendesk:
         """Create a new ticket and send the message to the customer. 
         Return the ID of the new ticket. 
         """
-        url = f'https://{self._subdomain}.zendesk.com/api/v2/tickets'
         data = {'ticket': {'subject': subject, 
                            'requester': {'email': customer_email, 'verified': True}, 
                            'assignee_email': assignee_email, 
-                           'comment': {'html_body': html_message, 'public': False}, 
-                           }}
-        response = requests.post(url, auth=self._auth, json=data)
+                           'comment': {'html_body': html_message, 'public': False}}}
+        response = requests.post(self._url, auth=self._auth, json=data)
         response.raise_for_status()
         ticket_id = str(response.json()['ticket']['id'])
 
-        response = requests.put(url + '/' + ticket_id, auth=self._auth, 
+        response = requests.put(self._url + '/' + ticket_id, auth=self._auth, 
                                 json={'ticket': {'comment': {'html_body': html_message, 'public': True}, 
                                                  'group_id': int(group_id), 
                                                  'tags': tags,
