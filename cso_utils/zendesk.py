bc3e0df04fdf71a09dfce1034089807a451784a8
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-03-18 16:22:32 -0700

    Return tickets created between specified dates

--- cso_utils/zendesk.py
+++ cso_utils/zendesk.py
@@ -1,10 +1,25 @@
+import datetime, time
+
 import requests
+
+
+class Ticket:
+    def __init__(self, json_data: dict):
+        self._data = json_data
+
+    def __repr__(self) -> str:
+        return f'Ticket({self._data})'
+
+    def data(self) -> dict:
+        """Return ticket data."""
+        return self._data
+
 
 class Zendesk:
     def authenticate(self, subdomain: str, email: str, token: str) -> None:
         self._subdomain = subdomain
         self._auth = (email + '/token', token)
-        self._url = f'https://{self._subdomain}.zendesk.com/api/v2/tickets'
+        self._url = f'https://{self._subdomain}.zendesk.com/api/v2/tickets.json'
 
     def create_ticket_and_send_to_customer(self, customer_email: str, subject: str, 
                                            html_message: str, group_id: str,
@@ -28,3 +43,23 @@ class Zendesk:
         response.raise_for_status()
 
         return ticket_id
+
+    def tickets_created_between_today_and(self, month: int, day: int, year: int) -> [Ticket]:
+        """Return a list of Ticket objects representing tickets created during the 
+        given times.
+        """
+        json_tickets = []
+        url = self._url + '?page[size]=100&sort=-id'
+        start_day = datetime.datetime(year, month, day)
+        while True:
+            response = requests.get(url, auth=self._auth)
+            response.raise_for_status()
+            response = response.json()
+            current = datetime.datetime.strptime(response['tickets'][0]['created_at'].split('T')[0], '%Y-%m-%d')
+            if current < start_day:
+                break
+            json_tickets.extend(response['tickets'])
+            url = response['links']['next']
+            time.sleep(1)
+
+        return [Ticket(ticket) for ticket in json_tickets]
