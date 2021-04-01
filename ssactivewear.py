6a0fdfc904424a27ecdbdda130b80292a9e5a984
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-03-31 17:01:22 -0700

    Create class that gets orders and requests returns

--- ssactivewear.py
+++ ssactivewear.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Order:
     def __init__(self, json_data: dict):
         self._data = json_data
@@ -8,3 +11,52 @@ class Order:
     def data(self) -> dict:
         """Return order data."""
         return self._data
+
+
+class SSActivewear:
+    def __init__(self, account: str, password: str):
+        self._auth = (account, password)
+        self._orders_endpoint = 'https://api.ssactivewear.com/v2/orders/'
+        self._returns_endpoint = 'https://api.ssactivewear.com/v2/returns/'
+        self._headers = {'Content-Type': 'application/json'}
+
+    def get_order(self, po_number: str) -> Order:
+        """Return an Order object representing the order with 
+        the given PO number. Ignore returns and cancellations.
+        """
+        response = requests.get(self._orders_endpoint + po_number + '?lines=true', 
+                                auth=self._auth, 
+                                headers=self._headers)
+        response.raise_for_status()
+        data = []
+        for package in response.json():
+            if (package['poNumber'] == po_number 
+                and package['orderType'] != 'Credit' 
+                and package['orderStatus'] != 'Cancelled'):
+                data.append(package)
+        return Order(data)
+
+    def full_return(self, po_number: str, reason_code: int, 
+                    reason_comment: str, test: bool) -> (str, dict):
+        """Request a full return. Return (RA number, shipping address)."""
+        original_order = self.get_order(po_number)
+        lines = []
+        for package in original_order.data():
+            for line in package['lines']:
+                lines.append({'invoiceNumber': package['invoiceNumber'], 
+                              'identifier': line['sku'], 
+                              'qty': line['qtyShipped'], 
+                              'showBoxes': False, 
+                              'returnReason': reason_code, 
+                              'isReplace': False, 
+                              'returnReasonComment': reason_comment})
+        data = {'emailConfirmation': '', 
+                'testOrder': test, 
+                'shippingLabelRequired': False, 
+                'lines': lines}
+        response = requests.post(self._returns_endpoint, 
+                                 auth=self._auth, 
+                                 json=data)
+        response.raise_for_status()
+        return_info = response.json()[0]['returnInformation']
+        return (return_info['raNumber'], return_info['returnToAddress'])
