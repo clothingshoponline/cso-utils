f6369f842f0199a79e7ade0344c2722567195a0f
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-04-02 11:19:05 -0700

    Add tests for Order class

--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
 from cso_utils import ssactivewear
 
+class TestOrder:
+    def test_repr(self):
+        order = ssactivewear.Order([{'A': 1}, {'B': 2}])
+        assert str(order) == "Order([{'A': 1}, {'B': 2}])"
+
+    def test_data(self):
+        order = ssactivewear.Order([{'A': 1}, {'B': 2}])
+        assert order.data() == [{'A': 1}, {'B': 2}]
+
 class TestSSActivewear:
 
     def test_filter(self):
