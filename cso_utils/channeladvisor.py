8003b9918115c2136debb7717a78d5101667b4fa
nazneenperacha <56941470+nazneenperacha@users.noreply.github.com>
2021-07-08 15:22:39 -0700

    Get order from channeladvisor using PO

--- cso_utils/channeladvisor.py
+++ cso_utils/channeladvisor.py
@@ -13,18 +13,23 @@ class ChannelAdvisorOrder:
 
     def po_number(self) -> str:
         """Return the PO Number."""
-        return str(self._data[0]['ID'])
+        return str(self._data['ID'])
 
 
 class ChannelAdvisor:
     def __init__(self, token: str):
         self._token = token
 
-    def get_order(self, site_order_id: str) -> ChannelAdvisorOrder:
+    def get_order(self, site_order_id_or_po: str) -> ChannelAdvisorOrder:
         """Return a ChannelAdvisorOrder object representing the order 
-        with the given order ID.
+        with the given order ID or PO number.
         """
-        endpoint = f"https://api.channeladvisor.com/v1/Orders?access_token={self._token}&$expand=Items,Fulfillments&$filter=SiteOrderID eq '{site_order_id}'"
+        if len(site_order_id_or_po) != 7:
+            endpoint = f"https://api.channeladvisor.com/v1/Orders?access_token={self._token}&$expand=Items,Fulfillments&$filter=SiteOrderID eq '{site_order_id_or_po}'"
+            response = requests.get(endpoint)
+            response.raise_for_status()
+            site_order_id_or_po = response.json()['value'][0]['ID']
+        endpoint = f"https://api.channeladvisor.com/v1/Orders({site_order_id_or_po})?access_token={self._token}&$expand=Items,Fulfillments"
         response = requests.get(endpoint)
         response.raise_for_status()
-        return ChannelAdvisorOrder(response.json()['value'])
+        return ChannelAdvisorOrder(response.json())
