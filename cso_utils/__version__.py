913a0aa21ff9872a6e53c0a7f4a74dd2b79ae62d
Nazneen Peracha <56941470+nazneenperacha@users.noreply.github.com>
2022-09-23 14:28:58 -0700

    Use python 3.10.7

--- cso_utils/__version__.py
+++ cso_utils/__version__.py
@@ -1,5 +1,5 @@
-__version__ = '6.0.0'
-__pyversion__ = '<=3.9.6'
+__version__ = '6.1.0'
+__pyversion__ = '<=3.10.7'
 __title__ = 'cso_utils'
 __author__ = 'Clothing Shop Online'
 __requests__ = 'requests<=2.25.1'
